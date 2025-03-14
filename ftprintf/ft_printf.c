2f44a731b0670aa561fc24dcaae7d4828a9bd31c
Klaudia Krasnodebska <kkrasnod@c3r16s2.42warsaw.pl>
2025-03-14 23:32:45 +0100

    test check draft

--- ftprintf/ft_printf.c
+++ ftprintf/ft_printf.c
@@ -1,17 +1,14 @@
-///* ************************************************************************** */
-///*                                                                            */
-///*                                                        :::      ::::::::   */
-///*   ft_printf.c                                        :+:      :+:    :+:   */
-///*                                                    +:+ +:+        
-	+:+     */
-///*   By: kkrasnod <kkrasnod@student.42.fr>          +#+  +:+      
-	+#+        */
-///*                                                +#+#+#+#+#+  
-	+#+           */
-///*   Created: 2025/01/10 15:30:30 by kkrasnod          #+#    #+#             */
-///*   Updated: 2025/03/09 16:54:14 by kkrasnod         ###   ########.fr       */
-///*                                                                            */
-///* ************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkrasnod <kkrasnod@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/14 21:01:52 by kkrasnod          #+#    #+#             */
+/*   Updated: 2025/03/14 23:31:20 by kkrasnod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 //#include "ft_printf.h"
 
@@ -152,12 +149,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <stdarg.h>
-
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int	print_char(int c)
 {
@@ -165,13 +160,15 @@ int	print_char(int c)
 	return (1);
 }
 
-int	print_string(char *c)
+int	print_string(char *s)
 {
 	int	count;
 
 	count = 0;
-	while (c[count])
-		write(1, &c[count++], 1);
+	if (!s)
+		return (write(1, "(null)", 6));
+	while (s[count])
+		write(1, &s[count++], 1);
 	return (count);
 }
 
@@ -180,10 +177,10 @@ int	print_pointer(unsigned long ptr)
 	int				count;
 	char			*base;
 	char			hex[17];
-	unsigned int	ui;
+	unsigned long	ui;
 	int				i;
 
-	ui = (unsigned int)ptr;
+	ui = ptr;
 	base = "0123456789abcdef";
 	i = 16;
 	count = 0;
@@ -203,34 +200,106 @@ int	print_pointer(unsigned long ptr)
 	return (count);
 }
 
+#include <unistd.h>
+
 int	print_decimal(int c)
 {
-	int temp;
-	int count;
-	int length;
-	char buffer[20];
+	int		count;
+	int		length;
+	char	buffer[12];
 
-	count = 0;
+	count = 10;
+	length = 0;
+	if (c == -2147483648)
+		return (write(1, "-2147483648", 11));
 	if (c == 0)
-		buffer[count++] = '0';
-
+		return (write(1, "0", 1));
 	if (c < 0)
 	{
 		write(1, "-", 1);
 		c = -c;
+		length++;
 	}
+	buffer[11] = '\0';
+	while (c)
+	{
+		buffer[count--] = (c % 10) + '0';
+		c /= 10;
+	}
+	length += write(1, &buffer[count + 1], 10 - count);
+	return (length);
+}
 
-	temp = c;
+int	print_undecimal(unsigned int u)
+{
+	unsigned	temp;
+	int			count;
+	int			lenght;
+	char		buffer[12];
+
+	count = 0;
+	temp = u;
+	if (temp == 0)
+		return (write(1, "0", 1));
 	while (temp > 0)
 	{
 		buffer[count++] = (temp % 10) + '0';
-		temp = temp / 10;
+		temp /= 10;
 	}
-	length = count;
-	while (--length >= 0)
-		write(1, &buffer[length], 1);
+	lenght = count;
+	while (--lenght >= 0)
+		write(1, &buffer[lenght], 1);
 	return (count);
 }
+
+int	print_heximal_small(int x)
+{
+	char			*hex;
+	char			buffer[16];
+	unsigned int	temp;
+	int				lenght;
+	int				count;
+
+	count = 0;
+	hex = "0123456789abcdef";
+	temp = (unsigned)x;
+	if (temp == 0)
+		return (write(1, "0", 1));
+	while (temp > 0)
+	{
+		buffer[count++] = hex[temp % 16];
+		temp /= 16;
+	}
+	lenght = count;
+	while (--lenght >= 0)
+		write(1, &buffer[lenght], 1);
+	return (count);
+}
+
+int	print_heximal_large(int X)
+{
+	char			*hex;
+	char			buffer[16];
+	unsigned int	temp;
+	int				lenght;
+	int				count;
+
+	count = 0;
+	hex = "0123456789ABCDEF";
+	temp = (unsigned)X;
+	if (temp == 0)
+		return (write(1, "0", 1));
+	while (temp > 0)
+	{
+		buffer[count++] = hex[temp % 16];
+		temp /= 16;
+	}
+	lenght = count;
+	while (--lenght >= 0)
+		write(1, &buffer[lenght], 1);
+	return (count);
+}
+
 int	check_next_char(va_list args, const char *s, int i)
 {
 	if (s[i] == 'c')
@@ -239,16 +308,16 @@ int	check_next_char(va_list args, const char *s, int i)
 		return (print_string(va_arg(args, char *)));
 	if (s[i] == 'p')
 		return (print_pointer((unsigned long)va_arg(args, void *)));
-// 	if (s[i] == 'd')
-// 		return (print_decimal(va_arg(args, int)));
-// 	if (s[i] == 'i')
-// 		return (print_integer(va_arg(args, int)));
-// 	if (s[i] == 'u')
-// 		return (print_undecimal(va_arg(args, unsigned int)));
-// 	if (s[i] == 'x')
-// 		return (print_heximal_small(va_arg(args, int)));
-// 	if (s[i] == 'X')
-// 		return (print_heximal_large(va_arg(args, int)));
+	if (s[i] == 'd' || s[i] == 'i')
+		return (print_decimal(va_arg(args, int)));
+	// if (s[i] == 'i')
+	//	return (print_integer(va_arg(args, int)));
+	if (s[i] == 'u')
+		return (print_undecimal(va_arg(args, unsigned int)));
+	if (s[i] == 'x')
+		return (print_heximal_small(va_arg(args, int)));
+	if (s[i] == 'X')
+		return (print_heximal_large(va_arg(args, int)));
 	if (s[i] == '%')
 		return (write(1, "%%", 1));
 	return (0);
@@ -281,12 +350,9 @@ int	ft_printf(const char *s, ...)
 	return (count);
 }
 
-int main()
-{
-    int number = 255;
-    number = 112;
-    ft_printf("moje %p", "test");
-    printf("\n%p", "test");
-
-    return 0;
-}
+// int	main(void)
+//{
+//	ft_printf("moje %p", "test");
+//	printf("\n%p\n", "test");
+//	return (0);
+//}
