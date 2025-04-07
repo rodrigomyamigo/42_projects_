735991d54e41fd4581051a0f7bfb57931697e84e
Klaudia Krasnodebska <kkrasnod@c2r6s4.42warsaw.pl>
2025-04-07 15:09:12 +0200

    fgt

--- getnextline/get_next_line.c
+++ getnextline/get_next_line.c
@@ -1,66 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkrasnod <kkrasnod@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 11:32:49 by kkrasnod          #+#    #+#             */
+/*   Updated: 2025/04/07 15:08:55 by kkrasnod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char	*join_and_free(const char *s1, const char *s2)
+char	*get_next_line(int fd)
 {
-	char	*temp;
-
-	temp = ft_strjoin(s1, s2);
-	free(s1);
-	return (temp);
-}
-
-char	*get_next_line(int fd) // chyba na razie giit
-{
-	static char *stash;
-	char *display_line;
+	char		*display_line;
+	static char	*stash;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	stash = fill_and_find_line(fd, stash);
+	stash = fill_and_find_newline(fd, stash);
 	if (!stash)
 		return (NULL);
-	// wiemy ze w stashu jest linia + potencjalnie cos jeszcze
-	// funkcja wprowadzajaca dane do linii
-	// funkcja usuwajaca ze stash'a wyswietlone juz linie
-	free(stash);
+	display_line = return_line(stash);
+	if (!display_line)
+		return (NULL);
+	stash = clean_stash(stash);
 	return (display_line);
 }
 
-char	*clean_stash_and_return(char *stash)
+char	*fill_and_find_newline(int fd, char *stash)
 {
-	int		found_after_;
-	int		i;
-	char	*temp;
-	char	*display_line;
-
-	i = 0;
-	temp = stash;
-	if (!stash)
-		return (NULL);
-	while (temp[i] && temp[i] != '\n')
-	{
-		i++;
-	}
-	display_line = (char *)malloc((ft_strlen(stash) - i) * sizeof(char));
-		// do zwryfikwoania
-	// temp = ft_strchr(stash, '\n');
-	while (temp[i] && temp[i] != '\n')
-	{
-		display_line[i] = temp[i];
-		i++;
-	}
-}
-
-char	*fill_and_find_line(int fd, char *stash)
-{
-	int		r;
 	char	*buffer;
+	int		r;
 
+	//buffer = (char *)ft_calloc((BUFFER_SIZE + 1), sizeof(char));
 	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
-	while ((r = read(fd, buffer, BUFFER_SIZE)) > 0)
+	r = 1;
+	while (r > 0)
 	{
+		r = read(fd, buffer, BUFFER_SIZE);
+		if (r == -1)
+		{
+			free(buffer);
+			return (NULL);
+		}
 		if (!stash)
 			stash = ft_strdup(buffer);
 		else
@@ -68,71 +54,146 @@ char	*fill_and_find_line(int fd, char *stash)
 		if (ft_strchr(stash, '\n'))
 			break ;
 	}
-	if (r == -1)
-		return (NULL);
 	free(buffer);
 	return (stash);
 }
+	//buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	//buffer = ft_calloc((BUFFER_SIZE + 1), sizeof(char));
 
-// char	*fill_and_find_line(int fd, char *stash)
+char	*join_and_free(char *s1, char *s2)
+{
+	char	*temp;
+
+	temp = ft_strjoin(s1, s2);
+	free(s1);
+	if (!temp)
+		return (NULL);
+	return (temp);
+}
+
+char	*clean_stash(char *stash)
+{
+	int		new_start;
+	char	*temp;
+	int		len;
+
+	len = 0;
+	while (stash[len])
+		len++;
+	new_start = 0;
+	if (!stash)
+		return (NULL);
+	while (stash[new_start] && stash[new_start] != '\n')
+		new_start++;
+	if (!stash[new_start])
+	{
+		free(stash);
+		return (NULL);
+	}
+	if (stash[new_start] == '\n')
+		new_start++;
+	temp = ft_substr(stash, new_start, len);
+	if (!temp)
+		return (NULL);
+	free(stash);
+	return (temp);
+}
+
+char	*return_line(char *stash)
+{
+	int		i;
+	char	*display_line;
+
+	i = 0;
+	if (!stash[i])
+		return (NULL);
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	display_line = (char *)malloc((i + 2) * sizeof(char));
+	if (!display_line)
+		return (NULL);
+	i = 0;
+	while (stash[i] && stash[i] != '\n')
+	{
+		display_line[i] = stash[i];
+		i++;
+	}
+	if (stash[i] == '\n')
+	{
+		display_line[i] = stash[i];
+		i++;
+	}
+	display_line[i] = '\0';
+	return (display_line);
+}
+
+//void	*ft_calloc(size_t count, size_t size)
 //{
-//	int	r;
-//	char	*buffer;
-//	char	*temp;
+//	void	*ptr;
+//	size_t	i;
 
-//	//temp = stash;
-//	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-//	if (!buffer)
+//	i = 0;
+//	ptr = malloc(count * size);
+//	if (!ptr)
 //		return (NULL);
-//	while ((r = read(fd, buffer, BUFFER_SIZE)) > 0)
+//	while (i < count * size)
 //	{
-//		if (!temp)
-//			temp = ft_strdup(buffer);
-//		else
-//			temp = ft_strjoin(temp, buffer);
-//		if (ft_strchr(stash, '\n'))
-//			break ;
+//		((unsigned char *)ptr)[i] = 0;
 //	}
-//	free(buffer);
-//	free(stash);
-//	return (temp);
+//	return (ptr);
 //}
 
-// char	*prepare_outcome(char *stash, int new_line_loc)
+//display_line = clean_stash_return_line(stash);
+//char	*clean_stash_return_line(char *stash)
 //{
-//	int		i;
+//	int		len;
+//	int		new_loc;
+//	char	*temp;
 //	char	*display_line;
 
-//	display_line = (char *)malloc((new_line_loc + 1) * sizeof(char));
-//	i = 0;
-//	while (stash[i] != '\n')
+//	if (!stash)
+//		return (NULL);
+//	len = ft_strlen(stash);
+//	new_loc = 0;
+//	while (stash[new_loc] && stash[new_loc] != '\n')
+//		new_loc++;
+//	display_line = (char *)malloc((new_loc + 2) * sizeof(char));
+//	temp = ft_substr(stash, new_loc, len);
+//	if (!temp)
+//		return (NULL);
+//	free(stash);
+//	new_loc = 0;
+//	while (temp[new_loc] && temp[new_loc] != '\n')
 //	{
-//		display_line[i] = stash[i];
-//		i++;
+//		display_line[new_loc] = temp[new_loc];
+//		new_loc++;
 //	}
-//	display_line[i] = stash[i];
-//	display_line[++i] = '\0';
-//	stash = ft_substr(stash, new_line_loc, ft_strlen(stash));
+//	display_line[new_loc] = '\0';
+//	stash = temp;
 //	return (display_line);
 //}
 
-// int	open_file(const char *fileName) /// dziala milo
-//{
-//	int file;
-//	if ((file = open(fileName, O_RDONLY)) == -1)
-//	{
-//		write(1, "Cannot open file\n", 17);
-//		return (1);
-//	}
-//	write(1, "File opened\n", 12);
-//	return (file); // zwraca fd
-//}
-
-int	main(void)
+ int	open_file(const char *fileName) /// dziala milo
 {
-	// char filename[] = "/nfs/homes/kkrasnod/student_Projects/github_libft/getnextline/t.txt";
-	// -> succes case
-	char filename[] = "book.txt"; // fail case -> no such file
-	// fd = open_file(filename);
-	return (0);
+	int file;
+	if ((file = open(fileName, O_RDONLY)) == -1)
+	{
+		write(1, "Cannot open file\n", 17);
+		return (1);
+	}
+	write(1, "File opened\n", 12);
+	return (file); // zwraca fd
 }
+
+// int	main(void)
+//{
+//	char filename[] ="/nfs/homes/kkrasnod/student_Projects/github_libft/getnextline/t.txt";
+//	// -> succes case
+//	// char filename[] = "book.txt"; // fail case -> no such file
+//	int fd = open_file(filename);
+//	char *line;
+//	int fd1;
+//	int fd2;
+
+//	return (0);
+//}
--- getnextline/get_next_line.h
+++ getnextline/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: kkrasnod <kkrasnod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 17:07:50 by kkrasnod          #+#    #+#             */
-/*   Updated: 2025/04/03 15:21:02 by kkrasnod         ###   ########.fr       */
+/*   Updated: 2025/04/07 14:54:41 by kkrasnod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,28 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5
+# define BUFFER_SIZE 5
 # endif
 
-//# include <fcntl.h> /// library for handling files --> open
+# include <fcntl.h> /// library for handling files --> open
 # include <stdlib.h>
 # include <unistd.h>
 
+// int		open_file(const char *fd); // DO USUNIECIA
+char	*clean_stash(char *stash);
+char	*fill_and_find_newline(int fd, char *stash);
+char	*join_and_free(char *s1, char *s2);
+char	*return_line(char *stash);
 char	*get_next_line(int fd);
-int		open_file(const char *fd);
+
+//char	*fill_and_find_newline(int fd, char *stash);
+
+//char	*clean_stash_return_line(char *stash);
+//void	*ft_calloc(size_t count, size_t size);
 //_utils
-char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
-int		ft_strlen(const char *s);
+//int		ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
-int		ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 
